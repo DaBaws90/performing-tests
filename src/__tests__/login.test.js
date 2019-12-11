afbfa8cd15e5b46fdf8295ae95f9387a9be0c8dd
Adrián <43435682+DaBaws90@users.noreply.github.com>
2019-12-11 12:12:41 +0100

    Configuring tests
    Setting up a test suite with some test-cases.

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,9 +1,13 @@
 import Login from '../views/Login'
 import { mount } from '@vue/test-utils'
 
+// Describe means is a test suite. test means is a test case
 describe('Login', () => {
+    const wrapper = mount(Login)
     test('Login instantiates properly', () => {
-        const wrapper = mount(Login)
         expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+    test('renders properly', () => { 
+        expect(wrapper.contains('form')).toBe(true)
     })
 })
--- src/__tests__/sum.test.js
+++ src/__tests__/sum.test.js
@@ -1,5 +1,5 @@
 describe('SUM', () => {
-    it('sum 1 + 2 equals 3', () => {
+    test.skip('sum 1 + 2 equals 3', () => {
         expect(1 + 2).toBe(3)
     })
 })
